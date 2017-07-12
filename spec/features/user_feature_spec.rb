38bf10fd073ea824985ae4d18e22cf6aa4a5e454
dlinch <dlinch33@gmail.com>
2017-07-11 18:59:54 -0600

    feature test for a user show

--- spec/features/user_feature_spec.rb
+++ spec/features/user_feature_spec.rb
@@ -4,8 +4,15 @@ describe 'seeing users' do
   let!(:user) { create(:user) }
 
   it 'displays the user page', js: true do
-    visit(users_path)
+    visit users_path
 
-    expect(page).to have_content 'Princess Bubblegum'
+    expect(page).to have_content user.full_name
+    expect(page).to have_content 'Users'
+  end
+
+  it 'displays a user page' do
+    visit user_path(user.id)
+
+    expect(page).to have_content user.full_name
   end
 end
