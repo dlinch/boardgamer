88b76cd6e4f053942ae16bcc7529cae49fe0c30a
dlinch <dlinch33@gmail.com>
2017-07-18 21:30:34 -0600

    updated factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,20 @@
 FactoryGirl.define do
   factory :user do
     first_name 'Princess'
-    last_name 'Bubblegum'
+    sequence(:last_name) { |n| "Bubblegum#{n}" }
+  end
+
+  factory :play do
+    player_ids { [build(:user), build(:user)] }
+  end
+
+  factory :win do
+    user
+    play
+    game
+  end
+
+  factory :game do
+    name 'Ticket To Ride'
   end
 end
