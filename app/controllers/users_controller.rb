fd98273129a6fe1e2599164be6aa557220fd5187
dlinch <dlinch33@gmail.com>
2017-09-03 20:21:00 -0600

    get user controller tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-     if user = User.create(params.require(:user).permit(:first_name, :last_name))
+     if user = User.create(user_params)
        redirect_to user_path(user.id)
      end
   end
@@ -20,15 +20,21 @@ class UsersController < ApplicationController
   end
 
   def update
-    update_params = params.require(:user).permit(:first_name, :last_name)
-    if user = User.find_by(id: params.id).update(params)
-      redirect_to user_path(user.first.id)
+    if user = User.find_by(id: params[:id])
+      user.update(user_params)
+      redirect_to user_path(user.id)
     end
   end
 
   def destroy
     if user = User.find_by(id: params[:id]).destroy
-      redirect_to root
+      redirect_to root_path
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -12,7 +12,7 @@ describe '#create' do
     let(:user_hash) { attributes_for(:user, first_name: 'Earl', last_name: 'Of Lemongrab') }
 
     it 'should create a new user' do
-      expect { get :create, params: {user: user_hash} }.to change { User.count }.from(0).to(1)
+      expect { post :create, params: {user: user_hash} }.to change { User.count }.by 1
       expect(User.last.full_name).to eq 'Earl Of Lemongrab'
     end
   end
@@ -46,19 +46,19 @@ describe '#create' do
     it 'should render the edit template for the specified user' do
       get :edit, params: {id: user.id}
 
-      expect(response).to render_template :index
+      expect(response).to render_template :edit
       expect(assigns(:user)).to eq user
     end
   end
 
   describe '#update' do
     let(:user) { create(:user) }
-    let(:user_update_hash) { {id: user.id, first_name: 'Magic', last_name: 'Man'} }
+    let(:user_update_hash) { {first_name: 'Magic', last_name: 'Man'} }
     let!(:user2) {create(:user, first_name: 'Marceline') }
 
     it 'should update the specified user' do
       expect(user.first_name).to eq 'Princess'
-      get :update, params: {user: user_update_hash}
+      patch :update, params: {user: user_update_hash, id: user.id}
 
       expect(user.reload.full_name).to eq 'Magic Man'
       expect(user2.reload.first_name).to eq 'Marceline'
@@ -66,9 +66,9 @@ describe '#create' do
   end
 
   describe '#destroy' do
-    let(:user) { create(:user) }
+    let!(:user) { create(:user) }
     it 'should destroy the specified user' do
-      expect { get :destroy, params: {id: user.id} }.to change { User.count }.from(1).to(0)
+      expect { delete :destroy, params: {id: user.id} }.to change { User.count }.by -1
     end
   end
 end
